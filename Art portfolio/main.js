dc4573a37981d91a9473d1dc6360c9bb120463ad
Tobi <tobiawolaju21@gmail.com>
2024-02-05 15:04:11 +0100

    hgsutds8z

--- Art portfolio/main.js
+++ Art portfolio/main.js
@@ -1,3 +1,4 @@
+// Scroll to the last item with animation
 window.onload = function () {
   var container = document.querySelector('.container');
   var lastItem = container.lastElementChild;
@@ -8,10 +9,36 @@ window.onload = function () {
   var fap3 = document.querySelector('.fap3');
   var fap3l = fap3.lastElementChild;
 
+  // Helper function for smooth scrolling
+  function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+      change = to - start,
+      currentTime = 0,
+      increment = 20;
 
-  // Scroll to the last item
-  container.scrollTop = lastItem.offsetTop;
-  fap1.scrollTop = fap1l.offsetTop;
-  fap2.scrollTop = fap2l.offsetTop;
-  fap3.scrollTop = fap3l.offsetTop;
+    var animateScroll = function () {
+      currentTime += increment;
+      var val = Math.easeInOutQuad(currentTime, start, change, duration);
+      element.scrollTop = val;
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+
+    animateScroll();
+  }
+
+  // Easing function for smooth scrolling
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+
+  // Scroll to the last item with animation
+  scrollTo(container, lastItem.offsetTop, 1000);
+  scrollTo(fap1, fap1l.offsetTop, 1500);
+  scrollTo(fap2, fap2l.offsetTop, 2500);
+  scrollTo(fap3, fap3l.offsetTop, 4500);
 };
