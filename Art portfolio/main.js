1cb15ae8b56e40f98de6e51ac5ec4461fc23a55b
Tobi <tobiawolaju21@gmail.com>
2024-02-23 00:59:32 +0100

    kjz

--- Art portfolio/main.js
+++ Art portfolio/main.js
@@ -41,7 +41,7 @@ function handleImage() {
   var blue = colorValues[2]/2;
   var backgroundColor = `rgba(${red}, ${green}, ${blue})`;  
   document.getElementById('content2').style.backgroundColor = backgroundColor;
-  var backgroundColor = `rgba(${red}, ${green}, ${blue},0.50)`;
+  var backgroundColor = `rgba(${red}, ${green}, ${blue},0.8)`;
   document.getElementById('view-works').style.backgroundColor = backgroundColor;
 
 
