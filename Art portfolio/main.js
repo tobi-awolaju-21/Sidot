d7485a1b4ac9ef4560c7cd91b86103aff068454a
Tobi <tobiawolaju21@gmail.com>
2024-02-22 23:51:34 +0100

    nbmn, vh m

--- Art portfolio/main.js
+++ Art portfolio/main.js
@@ -1,13 +1,4 @@
 
-var expectedBodyHeight = 150; // Set the expected height in vh
-var bodyHeight = expectedBodyHeight * window.innerHeight / 100;
-window.addEventListener('scroll', function() {
-  var scrolled = window.scrollY / (bodyHeight - window.innerHeight);
-  var scaleValue = 1 - (0.5 * scrolled);
-  // Ensure that scaleValue stays between 0.5 and 1
-  scaleValue = Math.max(0.5, Math.min(1, scaleValue));
-  document.getElementById('scalingImage').style.transform = 'scale(' + scaleValue + ') translateY(' + 0 + 'px)';
-});
 
 // get most used color.
 function handleImage() {
